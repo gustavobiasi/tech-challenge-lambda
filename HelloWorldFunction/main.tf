37767865a6a69b65320817478d4709f20d45e080
Gustavo Biasi <>
2025-03-23 15:42:03 -0300

    ajuste iam

--- HelloWorldFunction/main.tf
+++ HelloWorldFunction/main.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda-autenticacao" {
   function_name = "lambda-autenticacao"
   handler       = "helloworld.App::handleRequest"
   runtime       = "java17"
-  role          = "arn:aws:iam::141234920247:role/LabRole"
+  role          = "arn:aws:iam::960757163041:role/LabRole"
 
   filename         = "target/LambdaAutenticacao-1.0.jar"
   source_code_hash = filebase64sha256("target/LambdaAutenticacao-1.0.jar")
