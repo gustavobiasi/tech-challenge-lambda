7cc507d8aba741225c9497d23c4240b98f4b78ee
Gustavo Biasi <>
2025-03-23 15:18:08 -0300

    ajuste banco

--- HelloWorldFunction/src/main/java/helloworld/constants/Constants.java
+++ HelloWorldFunction/src/main/java/helloworld/constants/Constants.java
@@ -3,7 +3,7 @@ package helloworld.constants;
 public class Constants {
 
     public static String ALGORITHM_SECRET = "28fe7ea070610e704350937e55c4026b6de78942db924d2eb374701d33d8944a0260e9e82d7dced773d618a66ac189dcd4309192d5989f69d2662ef1a459f8ad00ddfe9d452a5a4f552c8a8df010765cae708b41d700ac6b2e5801f79b12fab6bcc8046dbcc8df73a0c4390de1062a4c93c7803b044410b3145c1caf706976a5b8134d7ee5a1f56b5db77a7b280c6fdfb0616407ecdda8dfc470249df9baa6646e0b5595826016957f08259456fdba24321a3e433e141a85fda69136e370f657189f82acd8d52920a49cd5f28434654831127648bfde98ddf948255df336c3492a148f819283dd093c239cc32a3ad7994b63a5ff8545afb2f1aa9475933e1967";
-    public static String DB_HOST = "fast-food-db.cz77kn37mgkf.us-east-1.rds.amazonaws.com";
+    public static String DB_HOST = "fast-food-db.cl1m5klpdfnd.us-east-1.rds.amazonaws.com";
     public static String DB_PORT = "5432";
     public static String DB_NAME = "fast_food";
     public static String DB_USER = "postgres";
